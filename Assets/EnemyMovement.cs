7588a7a1c524e0867abc53acf8ba10041cbab1b7
Harvey-G <harvey.gream@gmail.com>
2023-07-08 17:51:18 +0100

    Avert Warning	CS0108

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public CameraMove camera;
+    public new CameraMove camera;
     public bool is_Selected = false;
     private Vector3 mouseWorldPos;
     //public Vector3 position = transform.position;
