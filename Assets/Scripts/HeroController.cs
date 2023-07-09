b5693bbe5b657d246f414716d1356760ebec8ef6
Harvey-G <harvey.gream@gmail.com>
2023-07-09 10:45:15 +0100

    fix up Enemy and Hero Controllers and attacking

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -5,19 +5,23 @@ using System.Linq;
 
 public class HeroController : MonoBehaviour
 {
-    public int maxHealth;
-    public int health;
     [SerializeField] FloatingHealth healthBar;
-    public BoxCollider2D aim;
-    public float counter = 0;
-    public float attackForce;
-    //[SerializeField] Camera camera;
-    //[SerializeField] Transform hero;
+    public GameObject weaponHitboxPrefab;
+    private Collider2D weaponHitbox;
 
+    public int attackDamage;
+    public float attackForce;
+    public int maxHealth;
+
+    [SerializeField] int health;
+
+    private EnemyController closestEnemy;
+    private float counter = 0;
 
     private void Awake()
     {
         healthBar = GetComponentInChildren<FloatingHealth>();
+        weaponHitbox = Instantiate(weaponHitboxPrefab, transform).GetComponent<Collider2D>();
     }
 
     // Start is called before the first frame update
@@ -36,35 +40,30 @@ public class HeroController : MonoBehaviour
             counter = Mathf.Clamp(counter - Time.deltaTime, 0f, float.PositiveInfinity);
         //transform.rotation = camera.transform.rotation;
         //transform.position = target.transform.position + new(0, 75f, 0);
+
+        EnemyController[] enemies = FindObjectsByType<EnemyController>(FindObjectsSortMode.None);
+        closestEnemy = enemies.OrderBy(x => (x.transform.position - transform.position).sqrMagnitude).First();
+        if (closestEnemy == null) return;
+        weaponHitbox.transform.rotation = Quaternion.LookRotation(Vector3.forward, closestEnemy.transform.position - transform.position);
     }
 
     void OnAttack()
     {
-        Debug.Log("Attack");
-
-        EnemyController[] enemies = FindObjectsByType<EnemyController>(FindObjectsSortMode.None);
-        EnemyController closestEnemy = enemies.OrderBy(x => (x.transform.position - transform.position).sqrMagnitude).First();
-        if (closestEnemy == null) return;
-
         //adds force to rigidbodies
         Vector3 direction = closestEnemy.transform.position - transform.position;
         direction.Normalize();
 
         closestEnemy.GetComponent<Rigidbody2D>().AddForce(direction * attackForce);
-
+        closestEnemy.OnDamaged(attackDamage);
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
-
-        if (counter == 0)
+        if (!other.isTrigger && counter == 0)
         {
             OnAttack();
             counter = 2f;
         }
-
-
-
     }
 
     public void OnDamaged(int damageTaken)
